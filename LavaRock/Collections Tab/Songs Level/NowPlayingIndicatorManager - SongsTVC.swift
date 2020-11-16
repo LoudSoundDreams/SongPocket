725bdf6bac90b65b62c8eb17207673ca6bb84ac4
Linus <linus@loudsounddreams.com>
2020-11-16 10:39:02 -0800

    Fix bug: “now playing” indicator doesn’t appear on iOS 13 when player is playing.
    Released as version 1.0.1.

--- LavaRock/Collections Tab/Songs Level/NowPlayingIndicatorManager - SongsTVC.swift
+++ LavaRock/Collections Tab/Songs Level/NowPlayingIndicatorManager - SongsTVC.swift
@@ -17,9 +17,15 @@ extension SongsTVC: NowPlayingIndicatorManager {
 			rowMediaItem == playerController.nowPlayingItem
 		{
 			if playerController.playbackState == .playing { // There are many playback states; only show the "playing" icon when the player controller is playing. Otherwise, show the "not playing" icon.
-				return (
-					UIImage(systemName: "speaker.wave.2.fill"),
-					"Now playing")
+				if #available(iOS 14.0, *) {
+					return (
+						UIImage(systemName: "speaker.wave.2.fill"),
+						"Now playing")
+				} else { // iOS 13
+					return (
+						UIImage(systemName: "speaker.2.fill"),
+						"Now playing")
+				}
 			} else {
 				return (
 					UIImage(systemName: "speaker.fill"),
