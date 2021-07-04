0745c8ddcfc828a0a4cfd2af4b4f3dc9455a451e
Linus <linus@loudsounddreams.com>
2021-07-04 12:01:20 -0700

    Move startMovingAlbums() to the main AlbumsTVC file.

--- LavaRock/Albums Level/AlbumsTVC - “Moving Albums” Mode.swift
+++ LavaRock/Albums Level/AlbumsTVC - “Moving Albums” Mode.swift
@@ -10,54 +10,6 @@ import CoreData
 
 extension AlbumsTVC {
 	
-	// MARK: - Starting Moving Albums
-	
-	@objc func startMovingAlbums() {
-		
-		// Prepare a Collections view to present modally.
-		
-		guard
-			let modalCollectionsNC = storyboard!.instantiateViewController(withIdentifier: "Collections NC") as? UINavigationController,
-			let modalCollectionsTVC = modalCollectionsNC.viewControllers.first as? CollectionsTVC
-		else {
-			return
-		}
-		
-		// Initialize an AlbumMoverClipboard for the modal Collections view.
-		
-		guard let idOfSourceCollection = sectionOfLibraryItems.container?.objectID else { return }
-		
-		// Note the Albums to move, and to not move.
-		var idsOfAlbumsToMove = [NSManagedObjectID]()
-		var idsOfAlbumsToNotMove = [NSManagedObjectID]()
-		if tableView.indexPathsForSelectedRowsNonNil.isEmpty {
-			idsOfAlbumsToMove = sectionOfLibraryItems.items.map { $0.objectID }
-		} else {
-			for indexPath in indexPaths(forIndexOfSectionOfLibraryItems: 0) {
-				let album = libraryItem(for: indexPath)
-				if tableView.indexPathsForSelectedRowsNonNil.contains(indexPath) { // If the row is selected.
-					idsOfAlbumsToMove.append(album.objectID)
-				} else { // The row is not selected.
-					idsOfAlbumsToNotMove.append(album.objectID)
-				}
-			}
-		}
-		modalCollectionsTVC.albumMoverClipboard = AlbumMoverClipboard(
-			idOfSourceCollection: idOfSourceCollection,
-			idsOfAlbumsBeingMoved: idsOfAlbumsToMove,
-			idsOfAlbumsNotBeingMoved: idsOfAlbumsToNotMove,
-			delegate: self
-		)
-		
-		// Make the destination operate in a child managed object context, so that you can cancel without saving your changes.
-		let childManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-		childManagedObjectContext.parent = managedObjectContext
-		modalCollectionsTVC.managedObjectContext = childManagedObjectContext
-		
-		present(modalCollectionsNC, animated: true)
-		
-	}
-	
 	// MARK: - Ending Moving Albums
 	
 	@objc final func moveAlbumsHere() {
--- LavaRock/Albums Level/AlbumsTVC.swift
+++ LavaRock/Albums Level/AlbumsTVC.swift
@@ -87,6 +87,54 @@ final class AlbumsTVC:
 	@IBAction func unwindToAlbumsFromEmptyAlbum(_ unwindSegue: UIStoryboardSegue) {
 	}
 	
+	// MARK: - Starting Moving Albums
+	
+	@objc private func startMovingAlbums() {
+		
+		// Prepare a Collections view to present modally.
+		
+		guard
+			let modalCollectionsNC = storyboard!.instantiateViewController(withIdentifier: "Collections NC") as? UINavigationController,
+			let modalCollectionsTVC = modalCollectionsNC.viewControllers.first as? CollectionsTVC
+		else {
+			return
+		}
+		
+		// Initialize an AlbumMoverClipboard for the modal Collections view.
+		
+		guard let idOfSourceCollection = sectionOfLibraryItems.container?.objectID else { return }
+		
+		// Note the Albums to move, and to not move.
+		var idsOfAlbumsToMove = [NSManagedObjectID]()
+		var idsOfAlbumsToNotMove = [NSManagedObjectID]()
+		if tableView.indexPathsForSelectedRowsNonNil.isEmpty {
+			idsOfAlbumsToMove = sectionOfLibraryItems.items.map { $0.objectID }
+		} else {
+			for indexPath in indexPaths(forIndexOfSectionOfLibraryItems: 0) {
+				let album = libraryItem(for: indexPath)
+				if tableView.indexPathsForSelectedRowsNonNil.contains(indexPath) { // If the row is selected.
+					idsOfAlbumsToMove.append(album.objectID)
+				} else { // The row is not selected.
+					idsOfAlbumsToNotMove.append(album.objectID)
+				}
+			}
+		}
+		modalCollectionsTVC.albumMoverClipboard = AlbumMoverClipboard(
+			idOfSourceCollection: idOfSourceCollection,
+			idsOfAlbumsBeingMoved: idsOfAlbumsToMove,
+			idsOfAlbumsNotBeingMoved: idsOfAlbumsToNotMove,
+			delegate: self
+		)
+		
+		// Make the destination operate in a child managed object context, so that you can cancel without saving your changes.
+		let childManagedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+		childManagedObjectContext.parent = managedObjectContext
+		modalCollectionsTVC.managedObjectContext = childManagedObjectContext
+		
+		present(modalCollectionsNC, animated: true)
+		
+	}
+	
 	// MARK: - Refreshing Buttons
 	
 	final override func refreshBarButtons() {
@@ -104,7 +152,7 @@ final class AlbumsTVC:
 	
 	// MARK: - Navigation
 	
-	override func prepare(
+	final override func prepare(
 		for segue: UIStoryboardSegue,
 		sender: Any?
 	) {
