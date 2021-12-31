2852fa7442e9c51f91fae60f51878a5057331589
Linus <linus@loudsounddreams.com>
2021-12-30 22:02:50 -0800

    Fix a bug where the app wouldn’t reflect your theme until momentarily after appearing on launch. (Introduced during the migration to SwiftUI launch and life cycle.)
    Distributed as build 356.

--- LavaRock/Library/All Levels/LibraryNC.swift
+++ LavaRock/Library/All Levels/LibraryNC.swift
@@ -10,16 +10,28 @@ import UIKit
 final class LibraryNC: UINavigationController {
 	static let identifier = "Library NC"
 	
-	private static var didSetTheme = false
+	private static var didStartSettingTheme = false
+	final override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		if !Self.didStartSettingTheme {
+			Self.didStartSettingTheme = true
+			view.overrideUserInterfaceStyle = Appearance.savedPreference().uiUserInterfaceStyle()
+			view.tintColor = AccentColor.savedPreference().uiColor
+		}
+	}
 	
+	private static var didFinishSettingTheme = false
 	final override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
 		// Until `viewDidAppear`, `view.window == nil`.
-		if !Self.didSetTheme {
-			Self.didSetTheme = true
-			view.window?.overrideUserInterfaceStyle = Appearance.savedPreference().uiUserInterfaceStyle()
-			view.window?.tintColor = AccentColor.savedPreference().uiColor
+		if !Self.didFinishSettingTheme {
+			Self.didFinishSettingTheme = true
+			view.window?.overrideUserInterfaceStyle = view.overrideUserInterfaceStyle
+			view.overrideUserInterfaceStyle = .unspecified
+			view.window?.tintColor = view.tintColor
+			view.tintColor = nil
 		}
 	}
 }
