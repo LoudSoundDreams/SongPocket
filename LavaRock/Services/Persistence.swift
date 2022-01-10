f344b75367afde907e32ec0bb37f8bf42b661b2c
Linus <linus@loudsounddreams.com>
2022-01-10 13:39:44 -0800

    Clean up `Persistence` to match the fresh project template for a SwiftUI app.

--- LavaRock/Services/Persistence.swift
+++ LavaRock/Services/Persistence.swift
@@ -12,13 +12,8 @@ struct Persistence {
 	
 	static let viewContext = container.viewContext
 	private static let container: NSPersistentContainer = {
-		/*
-		 The persistent container for the application. This implementation
-		 creates and returns a container, having loaded the store for the
-		 application to it. This property is optional since there are legitimate
-		 error conditions that could cause the creation of the store to fail.
-		 */
 		let container = NSPersistentContainer(name: "LavaRock")
+		container.viewContext.automaticallyMergesChangesFromParent = true
 		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			if let error = error as NSError? {
 				// Replace this implementation with code to handle the error appropriately.
@@ -35,11 +30,6 @@ struct Persistence {
 				fatalError("Unresolved error \(error), \(error.userInfo)")
 			}
 		})
-		
-		
-		//		container.viewContext.mergePolicy = NSMergePolicy(merge: NSMergePolicyType.mergeByPropertyStoreTrumpMergePolicyType)
-		
-		
 		return container
 	}()
 }
