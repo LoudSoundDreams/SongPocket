accf1907a693a317c389bdcabe5693bbd7554975
Linus <linus@loudsounddreams.com>
2022-01-09 17:32:32 -0800

    Clean up minutiae.

--- LavaRock/Library/All Levels/LibraryTVC - Controlling Playback.swift
+++ LavaRock/Library/All Levels/LibraryTVC - Controlling Playback.swift
@@ -15,7 +15,7 @@ extension LibraryTVC {
 			playPauseButton.primaryAction = UIAction(
 				image: UIImage(systemName: "play.circle.fill")
 			) { _ in self.play() }
-//			playPauseButton.width = 10.0 // As of iOS 14.7.1, even when you set the width of each button manually, the "pause.fill" button is still narrower than the "play.fill" button.
+			// As of iOS 15.3 developer beta 1, even when you set `UIBarButtonItem.width` manually, the "pause.fill" button is still narrower than the "play.fill" button.
 			playPauseButton.accessibilityLabel = LocalizedString.play
 			playPauseButton.accessibilityTraits.formUnion(playButtonAdditionalAccessibilityTraits)
 		}
@@ -24,7 +24,6 @@ extension LibraryTVC {
 			playPauseButton.primaryAction = UIAction(
 				image: UIImage(systemName: "pause.circle.fill")
 			) { _ in self.pause() }
-//			playPauseButton.width = 10.0
 			playPauseButton.accessibilityLabel = LocalizedString.pause
 			playPauseButton.accessibilityTraits.subtract(playButtonAdditionalAccessibilityTraits)
 		}
@@ -42,29 +41,10 @@ extension LibraryTVC {
 		}
 		
 		// Enable or disable each button as appropriate
-		
+		playbackButtons.forEach { $0.enableWithAccessibilityTrait() }
 		if player.indexOfNowPlayingItem == 0 {
 			previousSongButton.disableWithAccessibilityTrait()
-		} else {
-			previousSongButton.enableWithAccessibilityTrait()
 		}
-		
-//		let currentPlaybackTime = sharedPlayer.currentPlaybackTime
-//		print(currentPlaybackTime)
-//		print(currentPlaybackTime == 0)
-//		print("")
-//		if
-//			sharedPlayer.currentPlaybackTime == 0,
-//			sharedPlayer.playbackState != .playing
-//		{
-//			rewindButton.disableWithAccessibilityTrait()
-//		} else {
-			rewindButton.enableWithAccessibilityTrait()
-//		}
-		
-		playPauseButton.enableWithAccessibilityTrait()
-		
-		nextSongButton.enableWithAccessibilityTrait()
 	}
 	
 	final override func accessibilityPerformMagicTap() -> Bool {
@@ -85,18 +65,11 @@ extension LibraryTVC {
 	}
 	
 	final func goToPreviousSong() {
-//		sharedPlayer?.currentPlaybackTime = 0 // Changing the "now playing" item triggers refreshPlaybackButtons(), but as of iOS 14.4 developer beta 1, without this line of code, we can actually finish all that before currentPlaybackTime actually changes to 0 for the new song, which causes us to not disable the "restart current song" button when we should.
-		// Actually, that line of code makes this method take forever to return; it repeatedly prints "SYNC-WATCHDOG-1: Attempting to wake up the remote process" and "SYNC-WATCHDOG-2: Tearing down connection". That happens whether we set currentPlaybackTime = 0 before or after changing the song.
-		
 		sharedPlayer?.skipToPreviousItem()
 	}
 	
 	final func rewind() {
 		sharedPlayer?.currentPlaybackTime = 0 // As of iOS 14.4 developer beta 1, skipToBeginning() doesn't reliably change currentPlaybackTime to 0, which causes us to not disable the "restart current song" when we should; but this line of code does.
-//		sharedPlayer?.skipToBeginning()
-//		sharedPlayer?.prepareToPlay()
-		
-//		refreshPlaybackButtons() // Disable the "restart current song" button if appropriate.
 	}
 	
 	private func play() {
@@ -108,8 +81,6 @@ extension LibraryTVC {
 	}
 	
 	final func goToNextSong() {
-//		sharedPlayer?.currentPlaybackTime = 0 // See corresponding comment in goToPreviousSong().
-		
 		sharedPlayer?.skipToNextItem()
 	}
 }
