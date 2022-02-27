73f98a14dac22a8d38fa334018aa310845f27f7e
Linus <linus@loudsounddreams.com>
2022-02-27 13:30:20 -0500

    Reorganize files.

---
+++ LavaRock/Helpers/Custom/LRUserDefaultsKey.swift
@@ -0,0 +1,14 @@
+//
+//  LRUserDefaultsKey.swift
+//  LavaRock
+//
+//  Created by h on 2022-02-27.
+//
+
+enum LRUserDefaultsKey: String, CaseIterable {
+	case lighting = "appearance"
+	case accentColor = "accentColorName"
+	
+	case hasEverImportedFromMusic = "hasEverImportedFromMusic"
+	case shouldExplainQueueAction = "shouldExplainQueueAction"
+}
--- LavaRock/Helpers/Foundation/UserDefaults.swift
+++ LavaRock/Helpers/Foundation/UserDefaults.swift
@@ -7,14 +7,6 @@
 
 import Foundation
 
-enum LRUserDefaultsKey: String, CaseIterable {
-	case lighting = "appearance"
-	case accentColor = "accentColorName"
-	
-	case hasEverImportedFromMusic = "hasEverImportedFromMusic"
-	case shouldExplainQueueAction = "shouldExplainQueueAction"
-}
-
 extension UserDefaults {
 	final func deleteAllValuesExceptForLRUserDefaultsKeys() {
 		let keysToKeep = Set(LRUserDefaultsKey.allCases.map { $0.rawValue })
