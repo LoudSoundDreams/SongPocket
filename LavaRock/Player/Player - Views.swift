c00b1f6b1181945ee8ff14e74e5675068f1a5011
Linus <linus@loudsounddreams.com>
2022-03-13 11:55:39 -0400

    Show “Then” and “Last” rows in queue

---
+++ LavaRock/Player/Player - Views.swift
@@ -0,0 +1,18 @@
+//
+//  Player - Views.swift
+//  LavaRock
+//
+//  Created by h on 2022-03-13.
+//
+
+import UIKit
+
+final class ThenModeCell: UITableViewCell {
+	@IBOutlet var segmentedControl: UISegmentedControl!
+	
+}
+
+final class LastModeCell: UITableViewCell {
+	@IBOutlet var segmentedControl: UISegmentedControl!
+	
+}
--- LavaRock/Player/PlayerVC.swift
+++ LavaRock/Player/PlayerVC.swift
@@ -41,21 +41,58 @@ extension PlayerVC: PlayerReflecting {
 extension PlayerVC: PlaybackToolbarManaging {}
 
 extension PlayerVC: UITableViewDataSource {
+	private enum RowCase: CaseIterable {
+		case then
+		case last
+		case song
+		
+		init(rowIndex: Int) {
+			switch rowIndex {
+			case 0:
+				self = .then
+			case 1:
+				self = .last
+			default:
+				self = .song
+			}
+		}
+	}
+	
 	final func tableView(
 		_ tableView: UITableView,
 		numberOfRowsInSection section: Int
 	) -> Int {
-		return SongQueue.songs.count
+		return SongQueue.songs.count + (RowCase.allCases.count - 1)
 	}
 	
 	final func tableView(
 		_ tableView: UITableView,
 		cellForRowAt indexPath: IndexPath
 	) -> UITableViewCell {
+		switch RowCase(rowIndex: indexPath.row) {
+		case .then:
+			guard let cell = tableView.dequeueReusableCell(
+				withIdentifier: "Then",
+				for: indexPath) as? ThenModeCell
+			else { return UITableViewCell() }
+			
+			return cell
+		case .last:
+			guard let cell = tableView.dequeueReusableCell(
+				withIdentifier: "Last",
+				for: indexPath) as? LastModeCell
+			else { return UITableViewCell() }
+			
+			return cell
+		case .song:
+			break
+		}
+		
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Song in Queue", for: indexPath)
 		
 		var content = UIListContentConfiguration.cell()
-		let metadatum = SongQueue.songs[indexPath.row].metadatum()
+		let metadatum = SongQueue.songs[indexPath.row - 2].metadatum()
+//		let metadatum = SongQueue.songs[indexPath.row].metadatum()
 		content.image = metadatum?.artworkImage(
 			at: CGSize(width: 5, height: 5))
 		content.text = metadatum?.titleOnDisk
@@ -68,6 +105,20 @@ extension PlayerVC: UITableViewDataSource {
 }
 
 extension PlayerVC: UITableViewDelegate {
+	final func tableView(
+		_ tableView: UITableView,
+		willSelectRowAt indexPath: IndexPath
+	) -> IndexPath? {
+		switch RowCase(rowIndex: indexPath.row) {
+		case .then:
+			return nil
+		case .last:
+			return nil
+		case .song:
+			return indexPath
+		}
+	}
+	
 	final func tableView(
 		_ tableView: UITableView,
 		didSelectRowAt indexPath: IndexPath
