b9d7e6735cab6a5fc1eb6a7efff04f0ca15c33c2
Linus <linus@loudsounddreams.com>
2022-04-25 00:41:10 -0400

    Add “restart” button to transport panel
    Distributed as build 503.

--- LavaRock/Console/TransportPanel.swift
+++ LavaRock/Console/TransportPanel.swift
@@ -17,61 +17,66 @@ struct TransportPanel: View {
 	private let eight: CGFloat = 8
 	private var player: MPMusicPlayerController? { TapeDeck.shared.player }
 	var body: some View {
-		HStack {
-			Button {
-				player?.skipToPreviousItem()
-			} label: {
-				Image(systemName: "arrowtriangle.up.circle")
-					.font(.system(size: eight * 4))
-			}
-			
-			Spacer()
-			
-			Button {
-				player?.currentPlaybackTime -= 10
-			} label: {
-				Image(systemName: "gobackward.10")
-					.font(.system(size: eight * 4))
-			}
-			
-			Spacer()
-			
-			Button {
-				guard let status = tapeDeckDisplay.currentStatus else { return }
-				if status.isInPlayMode {
-					player?.pause()
-				} else {
-					player?.play()
+		VStack {
+			HStack {
+				Button {
+					player?.skipToPreviousItem()
+				} label: {
+					Image(systemName: "arrowtriangle.up.circle")
+						.font(.system(size: eight * 4))
 				}
-			} label: {
-				if
-					let status = tapeDeckDisplay.currentStatus,
-					status.isInPlayMode
-				{
-					Image(systemName: "circle")
-						.font(.system(size: eight * 8))
-				} else {
-					Image(systemName: "circle.fill")
-						.font(.system(size: eight * 8))
+				Spacer()
+				Button {
+					player?.skipToBeginning()
+				} label: {
+					Image(systemName: "arrow.counterclockwise.circle")
+						.font(.system(size: eight * 4))
+				}
+				Spacer()
+				Button {
+					player?.skipToNextItem()
+				} label: {
+					Image(systemName: "arrowtriangle.down.circle")
+						.font(.system(size: eight * 4))
 				}
 			}
 			
-			Spacer()
+			Spacer(minLength: eight * 4)
 			
-			Button {
-				player?.currentPlaybackTime += 10
-			} label: {
-				Image(systemName: "goforward.10")
-					.font(.system(size: eight * 4))
-			}
-			
-			Spacer()
-			
-			Button {
-				player?.skipToNextItem()
-			} label: {
-				Image(systemName: "arrowtriangle.down.circle")
-					.font(.system(size: eight * 4))
+			HStack {
+				Button {
+					player?.currentPlaybackTime -= 10
+				} label: {
+					Image(systemName: "gobackward.10")
+						.font(.system(size: eight * 4))
+				}
+				Spacer()
+				Button {
+					guard let status = tapeDeckDisplay.currentStatus else { return }
+					if status.isInPlayMode {
+						player?.pause()
+					} else {
+						player?.play()
+					}
+				} label: {
+					if
+						let status = tapeDeckDisplay.currentStatus,
+						status.isInPlayMode
+					{
+						Image(systemName: "circle")
+							.font(.system(size: eight * 8))
+					} else {
+						Image(systemName: "circle.fill")
+							.font(.system(size: eight * 8))
+					}
+				}
+				Spacer()
+				Button {
+					player?.currentPlaybackTime += 10
+				} label: {
+					Image(systemName: "goforward.10")
+						.font(.system(size: eight * 4))
+				}
 			}
 		}
 		.padding([.top, .bottom], eight * 6)
