a7814b94f6897690b94c4cfe5e9d4d4701847bb8
Linus <linus@loudsounddreams.com>
2022-04-28 03:06:32 -0400

    Clean up minutiae

--- LavaRock/Enabling.swift
+++ LavaRock/Enabling.swift
@@ -21,7 +21,7 @@ struct Enabling {
 	)
 	static let jumpButtons = console ? (10 == 10) : (10 == 1)
 	static let popoverConsole = console && transportToolbar && !jumpButtons && 10 == 10
-	static let optionsInTabBar = console && !transportToolbar
+	static let optionsInTabBar = console && !transportToolbar && 10 == 1
 	static let swiftUI__console = console && 10 == 1
 	
 	static let swiftUI__options = 10 == 1
