e2944655362b45902dc3a8faf8744fc588f60181
Linus <linus@loudsounddreams.com>
2022-07-01 00:09:37 -0400

    Refile code

---
+++ LavaRock/MusicFolder/Sim_SongMetadatum.swift
@@ -0,0 +1,95 @@
+//
+//  Sim_SongMetadatum.swift
+//  LavaRock
+//
+//  Created by h on 2022-06-30.
+//
+
+#if targetEnvironment(simulator)
+import MediaPlayer
+import os
+
+struct Sim_AlbumIDDispenser {
+	private static var nextAvailable = 1
+	static func takeNumber() -> AlbumID {
+		let result = AlbumID(nextAvailable)
+		nextAvailable += 1
+		return result
+	}
+}
+private struct Sim_SongIDDispenser {
+	private static var nextAvailable = 1
+	static func takeNumber() -> SongID {
+		let result = SongID(nextAvailable)
+		nextAvailable += 1
+		return result
+	}
+}
+struct Sim_SongMetadatum: SongMetadatum {
+	// `SongMetadatum`
+	
+	let albumID: AlbumID
+	let songID: SongID
+	
+	let albumArtistOnDisk: String?
+	let albumTitleOnDisk: String?
+	
+	let discCountOnDisk: Int
+	let discNumberOnDisk: Int
+	let trackNumberOnDisk: Int
+	static let unknownTrackNumber = MPMediaItem.unknownTrackNumber
+	
+	let titleOnDisk: String?
+	let artistOnDisk: String?
+	
+	let dateAddedOnDisk: Date
+	let releaseDateOnDisk: Date?
+	
+	func coverArt(sizeInPoints: CGSize) -> UIImage? {
+		let signposter = OSSignposter()
+		let state = signposter.beginInterval("Sim: draw cover art")
+		defer {
+			signposter.endInterval("Sim: draw cover art", state)
+		}
+		guard let fileName = coverArtFileName else {
+			return nil
+		}
+		return UIImage(named: fileName)
+	}
+	
+	private let coverArtFileName: String?
+}
+extension Sim_SongMetadatum {
+	static var dict: [SongID: Self] = [:]
+	
+	init(
+		albumID: AlbumID,
+		albumArtistOnDisk: String?,
+		albumTitleOnDisk: String?,
+		discCountOnDisk: Int,
+		discNumberOnDisk: Int,
+		trackNumberOnDisk: Int,
+		titleOnDisk: String?,
+		artistOnDisk: String?,
+		dateAddedOnDisk: Date,
+		releaseDateOnDisk: Date?,
+		coverArtFileName: String?
+	) {
+		self.init(
+			albumID: albumID,
+			songID: Sim_SongIDDispenser.takeNumber(),
+			albumArtistOnDisk: albumArtistOnDisk,
+			albumTitleOnDisk: albumTitleOnDisk,
+			discCountOnDisk: discCountOnDisk,
+			discNumberOnDisk: discNumberOnDisk,
+			trackNumberOnDisk: trackNumberOnDisk,
+			titleOnDisk: titleOnDisk,
+			artistOnDisk: artistOnDisk,
+			dateAddedOnDisk: dateAddedOnDisk,
+			releaseDateOnDisk: releaseDateOnDisk,
+			coverArtFileName: coverArtFileName)
+		
+		Self.dict[self.songID] = self
+	}
+}
+#endif
--- LavaRock/MusicFolder/SongMetadatum.swift
+++ LavaRock/MusicFolder/SongMetadatum.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import MediaPlayer
 import os
 
 typealias AlbumID = Int64
@@ -32,122 +31,6 @@ protocol SongMetadatum {
 	
 	func coverArt(sizeInPoints: CGSize) -> UIImage?
 }
-
-extension MPMediaItem: SongMetadatum {
-	final var albumID: AlbumID { AlbumID(bitPattern: albumPersistentID) }
-	final var songID: SongID { SongID(bitPattern: persistentID) }
-	
-	// Media Player reports unknown values as …
-	
-	final var albumArtistOnDisk: String? { albumArtist } // … `nil`, as of iOS 14.7 developer beta 5.
-	final var albumTitleOnDisk: String? { albumTitle } // … `""`, as of iOS 14.7 developer beta 5.
-	
-	final var discCountOnDisk: Int { discCount } // … `0`, as of iOS 15.0 RC.
-	final var discNumberOnDisk: Int { discNumber } // … `1`, as of iOS 14.7 developer beta 5.
-	final var trackNumberOnDisk: Int { albumTrackNumber }
-	static let unknownTrackNumber = 0 // As of iOS 14.7 developer beta 5.
-	
-	final var titleOnDisk: String? { title } // … we don’t know, because Music for Mac as of version 1.1.5.74 doesn’t allow blank song titles. But that means we shouldn’t need to move unknown song titles to the end.
-	final var artistOnDisk: String? { artist }
-	
-	final var dateAddedOnDisk: Date { dateAdded }
-	final var releaseDateOnDisk: Date? { releaseDate }
-	
-	func coverArt(sizeInPoints: CGSize) -> UIImage? {
-		let signposter = OSSignposter()
-		let state = signposter.beginInterval("Draw cover art")
-		defer {
-			signposter.endInterval("Draw cover art", state)
-		}
-		return artwork?.image(at: sizeInPoints)
-	}
-}
-
-#if targetEnvironment(simulator)
-struct Sim_AlbumIDDispenser {
-	private static var nextAvailable = 1
-	static func takeNumber() -> AlbumID {
-		let result = AlbumID(nextAvailable)
-		nextAvailable += 1
-		return result
-	}
-}
-private struct Sim_SongIDDispenser {
-	private static var nextAvailable = 1
-	static func takeNumber() -> SongID {
-		let result = SongID(nextAvailable)
-		nextAvailable += 1
-		return result
-	}
-}
-struct Sim_SongMetadatum: SongMetadatum {
-	// `SongMetadatum`
-	
-	let albumID: AlbumID
-	let songID: SongID
-	
-	let albumArtistOnDisk: String?
-	let albumTitleOnDisk: String?
-	
-	let discCountOnDisk: Int
-	let discNumberOnDisk: Int
-	let trackNumberOnDisk: Int
-	static let unknownTrackNumber = MPMediaItem.unknownTrackNumber
-	
-	let titleOnDisk: String?
-	let artistOnDisk: String?
-	
-	let dateAddedOnDisk: Date
-	let releaseDateOnDisk: Date?
-	
-	func coverArt(sizeInPoints: CGSize) -> UIImage? {
-		let signposter = OSSignposter()
-		let state = signposter.beginInterval("Sim: draw cover art")
-		defer {
-			signposter.endInterval("Sim: draw cover art", state)
-		}
-		guard let fileName = coverArtFileName else {
-			return nil
-		}
-		return UIImage(named: fileName)
-	}
-	
-	private let coverArtFileName: String?
-}
-extension Sim_SongMetadatum {
-	static var dict: [SongID: Self] = [:]
-	
-	init(
-		albumID: AlbumID,
-		albumArtistOnDisk: String?,
-		albumTitleOnDisk: String?,
-		discCountOnDisk: Int,
-		discNumberOnDisk: Int,
-		trackNumberOnDisk: Int,
-		titleOnDisk: String?,
-		artistOnDisk: String?,
-		dateAddedOnDisk: Date,
-		releaseDateOnDisk: Date?,
-		coverArtFileName: String?
-	) {
-		self.init(
-			albumID: albumID,
-			songID: Sim_SongIDDispenser.takeNumber(),
-			albumArtistOnDisk: albumArtistOnDisk,
-			albumTitleOnDisk: albumTitleOnDisk,
-			discCountOnDisk: discCountOnDisk,
-			discNumberOnDisk: discNumberOnDisk,
-			trackNumberOnDisk: trackNumberOnDisk,
-			titleOnDisk: titleOnDisk,
-			artistOnDisk: artistOnDisk,
-			dateAddedOnDisk: dateAddedOnDisk,
-			releaseDateOnDisk: releaseDateOnDisk,
-			coverArtFileName: coverArtFileName)
-		
-		Self.dict[self.songID] = self
-	}
-}
-#endif
 
 struct SongMetadatumPlaceholder {
 	static let unknownTitle = "—" // Em dash
