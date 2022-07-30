c8fdbddd2121212717e562d5afebd11a4e872559
Linus <linus@loudsounddreams.com>
2022-07-30 18:56:54 -0400

    Disable large title in songs view

--- LavaRock/Library/Songs/SongsTVC.swift
+++ LavaRock/Library/Songs/SongsTVC.swift
@@ -32,6 +32,12 @@ final class SongsTVC:
 		]
 	}
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		navigationItem.largeTitleDisplayMode = .never
+	}
+	
 	override func setUpBarButtons() {
 		editingModeToolbarButtons = [
 			sortButton,
