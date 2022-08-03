2afe52eac9828d3052d8abeccb715aa0b8511db3
Linus <linus@loudsounddreams.com>
2022-08-03 01:30:09 -0400

    Improve accent colors in light mode

--- LavaRock/General UI/AccentColor.swift
+++ LavaRock/General UI/AccentColor.swift
@@ -67,15 +67,112 @@ enum AccentColor: String, CaseIterable {
 	var uiColor: UIColor {
 		switch self {
 		case .strawberry:
-			return .systemPink
+			/*
+			 Hue:
+			 • 335 - artificially pink
+			 • 350 - boring “classic red”
+			 
+			 Saturation:
+			 • 0.80 - too desaturated
+			 • 0.90 - dark mode: too garish
+			 
+			 Brightness:
+			 • 0.80 - dark mode: too dark
+			 • 1.00 - boringly bright
+			 */
+			return UIColor( // Good in light and dark mode
+				hue: 340/360, // Aiming low
+				saturation: 0.85,
+				brightness: 0.95, // Aiming high
+				alpha: 1)
 		case .tangerine:
-			return .systemOrange
+			/*
+			 Hue:
+			 • 25 - boring orange (the fruit)
+			 • 35 - GOOD
+			 • 40 - yellow
+			 
+			 Saturation:
+			 • 0.75 - too desaturated
+			 • 1.00 - still not garish
+			 
+			 Brightness:
+			 • 0.90 - light mode: GOOD. dark mode: too dark
+			 •
+			 */
+			return UIColor( // Good in light mode, bad in dark mode
+				hue: 35/360, // Aiming high
+				saturation: 1.00, // Aiming high
+				brightness: 0.90, // Aiming low
+				alpha: 1)
 		case .lime:
-			return .systemGreen
+			/*
+			 Hue:
+			 • 80 - browning guac
+			 • 90 - annoyingly yellow
+			 • 105 - distractingly yellow
+			 
+			 • 130 - canonical (boring) green, to me. i like yellower greens better here than bluer ones
+			 • 145 - too blue
+			 
+			 Saturation:
+			 •
+			 
+			 Brightness:
+			 • 0.55 - light mode: annoyingly dark
+			 • 0.75 - light mode: annoyingly bright
+			 */
+			return UIColor( // Good in light mode, bad in dark mode
+				hue: 110/360, // Aiming low
+				saturation: 1.00, // Aiming high
+				brightness: 0.60, // Aiming low
+				alpha: 1)
 		case .blueberry:
-			return .systemBlue
+			/*
+			 Hue:
+			 • 210 - approaching cyan
+			 • 215 - GOOD. a tinge of yellow
+			 • 235 - approaching purple
+			 
+			 # For hue 215
+			 
+			 Saturation:
+			 • 0.80 - too desaturated
+			 • 0.90 - dark mode: garish at any brightness
+			 
+			 Brightness:
+			 • 0.65 - light mode: annoyingly dark
+			 • 0.90 - dark mode: too dark
+			 */
+			return UIColor( // Good in light mode, bad in dark mode
+				hue: 215/360, // Aiming low
+				saturation: 1.00, // Aiming high
+				brightness: 0.75, // Aiming low
+				alpha: 1)
 		case .grape:
-			return .systemPurple
+			/*
+			 Hue:
+			 • 280 - boring lavender
+			 
+			 • 315 - too close to Strawberry
+			 • 325 - approaching maroon or pink
+			 
+			 Saturation:
+			 • 0.60 - too desaturated
+			 • 0.90 - dark mode: too garish
+			 
+			 Brightness:
+			 • 0.60 - light mode: nice and dark, barely different from black
+			 
+			 • 0.70 - dark mode: too dark
+			 
+			 • 0.95 - light mode: annoyingly bright. dark mode: boringly bright
+			 */
+			return UIColor( // Good in light mode, bad in dark mode
+				hue: 310/360, // Aiming high
+				saturation: 0.85, // Aiming high
+				brightness: 0.65, // Aiming low
+				alpha: 1)
 		}
 	}
 	
