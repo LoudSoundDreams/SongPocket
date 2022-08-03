167e20ea3a193009143d22cc4c9023098aee1e0f
Linus <linus@loudsounddreams.com>
2022-08-03 09:47:01 -0400

    Desaturate “strawberry”

--- LavaRock/General UI/AccentColor.swift
+++ LavaRock/General UI/AccentColor.swift
@@ -90,7 +90,7 @@ enum AccentColor: String, CaseIterable {
 				alpha: 1)
 			return UIColor( // Good in dark mode
 				hue: 340/360, // Aiming low
-				saturation: 0.90, // Aiming high
+				saturation: 0.80, // Aiming high
 				brightness: 1.00, // Aiming high
 				alpha: 1)
 			 */
