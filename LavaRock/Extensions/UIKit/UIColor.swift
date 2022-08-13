e26cd83e8a3ba1b6b5539245f6846fa459773eef
Linus <linus@loudsounddreams.com>
2022-08-13 18:49:49 -0400

    Add helper methods for debugging

--- LavaRock/Extensions/UIKit/UIColor.swift
+++ LavaRock/Extensions/UIKit/UIColor.swift
@@ -8,6 +8,38 @@
 import UIKit
 
 extension UIColor {
+	final func printHSBA() {
+		var mutableHue: CGFloat = .zero
+		var mutableSaturation: CGFloat = .zero
+		var mutableBrightness: CGFloat = .zero
+		var mutableAlpha: CGFloat = .zero
+		getHue(
+			&mutableHue,
+			saturation: &mutableSaturation,
+			brightness: &mutableBrightness,
+			alpha: &mutableAlpha)
+		print("H:", mutableHue)
+		print("S:", mutableSaturation)
+		print("B:", mutableBrightness)
+		print("A:", mutableAlpha)
+	}
+	
+	final func printRGBA() {
+		var mutableRed: CGFloat = .zero
+		var mutableGreen: CGFloat = .zero
+		var mutableBlue: CGFloat = .zero
+		var mutableAlpha: CGFloat = .zero
+		getRed(
+			&mutableRed,
+			green: &mutableGreen,
+			blue: &mutableBlue,
+			alpha: &mutableAlpha)
+		print("R:", mutableRed)
+		print("G:", mutableGreen)
+		print("B:", mutableBlue)
+		print("A:", mutableAlpha)
+	}
+	
 	final func translucentFaint() -> UIColor {
 		return withAlphaComponent(.oneEighth)
 	}
