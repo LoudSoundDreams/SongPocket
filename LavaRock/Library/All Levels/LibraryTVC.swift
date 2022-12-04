a048c8cad5c33e1dabaeb2b7da6a424067f1b713
Linus <linus@loudsounddreams.com>
2022-12-04 01:15:15 -0500

    Delete unnecessary table view optimization which might be causing a bug

--- LavaRock/Library/All Levels/LibraryTVC.swift
+++ LavaRock/Library/All Levels/LibraryTVC.swift
@@ -175,7 +175,7 @@ class LibraryTVC: UITableViewController {
 			reflectViewContainer()
 			// Update the data within each row (and header), which might be outdated.
 			// Doing it without an animation looks fine, because we animated the deletes, inserts, and moves earlier; here, we just change the contents of the rows after they stop moving.
-			tableView.reconfigureRows(at: tableView.visibleIndexPaths)
+			tableView.reconfigureRows(at: tableView.allIndexPaths())
 		}
 	}
 	
