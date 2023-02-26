6d15961684e0dacd93cd0c76f60f031c7f361a14
Linus <linus@loudsounddreams.com>
2023-02-25 19:58:05 -0500

    Revert "Require 2 items to sort"
    This reverts commit 74a136cd745cb7494f51e63e4100fa70bf8b2bc9.

--- LavaRock/Library/All Levels/LibraryTVC.swift
+++ LavaRock/Library/All Levels/LibraryTVC.swift
@@ -408,13 +408,6 @@ class LibraryTVC: UITableViewController {
 				return false
 			}
 			let selectedIndexPaths = tableView.selectedIndexPaths
-			
-			let to_sort = viewModel.indexPaths_for_all_if_empty_else_unsorted(
-				selectedIndexPaths: selectedIndexPaths)
-			guard to_sort.count >= 2 else {
-				return false
-			}
-			
 			if selectedIndexPaths.isEmpty {
 				return true
 			} else {
