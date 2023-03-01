3e2380bd3149b693a45fcf313c3e2f43898b3b6d
Linus <linus@loudsounddreams.com>
2023-03-01 01:50:35 -0500

    Redesign album info row
    Build 712

--- LavaRock/Library/Songs/AlbumInfoRow.swift
+++ LavaRock/Library/Songs/AlbumInfoRow.swift
@@ -23,38 +23,32 @@ struct AlbumInfoRow: View {
 	}
 	
 	var body: some View {
-		VStack(
-			spacing: .eight // 8
-		) {
-			// “Please Please Me”
-			Text(albumTitle)
-				.multilineTextAlignment(.center)
-				.font(.title2)
-				.fontWeight(.bold)
-			
-			// Subtitle
-			Group {
-				// Concatenate instances of `Text`
+		HStack {
+			VStack(
+				alignment: .leading,
+				spacing: .eight * 5/8
+			) {
+				// “Please Please Me”
+				Text(albumTitle)
+					.font(.title2)
+					.fontWeight(.bold)
 				
 				// “The Beatles”
 				Text(albumArtist)
-				.fontWeight(.bold) // As of iOS 16.2 developer beta 4, this is thicker than `.bold()`, peculiarly
-				+
+					.fontWeight(.bold) // As of iOS 16.2 developer beta 4, this is thicker than `.bold()`, peculiarly
+					.font(.caption)
+					.foregroundColor(.secondary)
 				
-				// “· Mar 22, 1963”
-				Text({ () -> String in
-					guard let releaseDateString else {
-						return ""
-					}
-					return " " + LRString.interpunct + " " + releaseDateString
-				}())
+				if let releaseDateString {
+					// “Mar 22, 1963”
+					Text(releaseDateString)
+						.font(.caption)
+						.foregroundColor(.secondary)
+				}
 			}
-			.multilineTextAlignment(.center)
-			.font(.caption)
-			.foregroundColor(.secondary)
+			
+			Spacer()
 		}
-		.padding(.top, .eight * -1/4) // -2
-		.padding(.bottom, .eight * 3/8) // 3
-		.frame(maxWidth: .infinity)
+		.padding(.bottom, .eight * 4/8)
 	}
 }
