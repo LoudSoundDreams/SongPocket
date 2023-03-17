777a77e8cbb1fdfbe714cee4e93fd09ef1a057ae
Linus <linus@loudsounddreams.com>
2023-03-17 01:33:19 -0400

    Rearrange code

--- LavaRock/Console/ConsoleVC.swift
+++ LavaRock/Console/ConsoleVC.swift
@@ -60,3 +60,25 @@ final class ConsoleVC: UIViewController {
 		return player.indexOfNowPlayingItem == indexPath.row
 	}
 }
+extension ConsoleVC: UITableViewDataSource {
+	func tableView(
+		_ tableView: UITableView,
+		numberOfRowsInSection section: Int
+	) -> Int {
+		return Reel.mediaItems.count
+	}
+	
+	func tableView(
+		_ tableView: UITableView,
+		cellForRowAt indexPath: IndexPath
+	) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(
+			withIdentifier: "Song in Queue",
+			for: indexPath) as? QueueCell
+		else { return UITableViewCell() }
+		
+		cell.configure(with: Reel.mediaItems[indexPath.row])
+		
+		return cell
+	}
+}
