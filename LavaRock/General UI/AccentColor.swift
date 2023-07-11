89da2c8833e76019e33fb44441fedcc0c3953254
Linus <linus@loudsounddreams.com>
2023-07-11 02:33:59 -0400

    Rearrange code

--- LavaRock/General UI/AccentColor.swift
+++ LavaRock/General UI/AccentColor.swift
@@ -37,36 +37,92 @@ enum AccentColor: CaseIterable {
 	
 	var displayName: String {
 		switch self {
+			case .blueberry:
+				return LRString.blueberry
+			case .grape:
+				return LRString.grape
 			case .strawberry:
 				return LRString.strawberry
 			case .tangerine:
 				return LRString.tangerine
 			case .lime:
 				return LRString.lime
-			case .blueberry:
-				return LRString.blueberry
-			case .grape:
-				return LRString.grape
 		}
 	}
 	
 	var color: Color {
 		switch self {
+			case .blueberry:
+				return .blue
+			case .grape:
+				return .purple
 			case .strawberry:
 				return .pink
 			case .tangerine:
 				return .orange
 			case .lime:
 				return .green
-			case .blueberry:
-				return .blue
-			case .grape:
-				return .purple
 		}
 	}
 	
 	var uiColor: UIColor {
 		switch self {
+			case .blueberry:
+				/*
+				 Hue:
+				 • 200 - approaching cyan
+				 • 215 - GOOD. a tinge of yellow
+				 • 235 - approaching purple
+				 
+				 # For hue 215
+				 
+				 Saturation:
+				 • 0.80 - too desaturated
+				 • 0.90 - dark mode: garish at any brightness
+				 
+				 Brightness:
+				 • 0.65 - light mode: annoyingly dark
+				 • 0.90 - dark mode: too dark
+				 */
+//				return UIColor( // Good in light mode, bad in dark mode
+//					hue: 215/360, // Aiming low
+//					saturation: 1.00, // Aiming high
+//					brightness: 0.75, // Aiming low
+//					alpha: 1)
+//				return UIColor( // Good in dark mode
+//					hue: 210/360, // Aiming low
+//					saturation: 0.90, // Aiming high
+//					brightness: 1.00, // Aiming high
+//					alpha: 1)
+				return UIColor(named: "blueberry")!
+			case .grape:
+				/*
+				 Hue:
+				 • 255 - too blue
+				 • 280 - boring lavender
+				 
+				 • 315 - too close to Strawberry
+				 • 325 - approaching maroon
+				 
+				 Saturation:
+				 • 0.60 - too desaturated
+				 • 0.90 - dark mode: too garish
+				 
+				 Brightness:
+				 • 0.60 - light mode: barely different from black
+				 • 0.95 - light mode: annoyingly bright. dark mode: boringly bright
+				 */
+//				return UIColor( // Good in light mode, bad in dark mode
+//					hue: 310/360, // Aiming high
+//					saturation: 1.00, // Aiming high
+//					brightness: 0.65, // Aiming low
+//					alpha: 1)
+//				return UIColor( // Good in dark mode
+//					hue: 310/360, // Aiming high
+//					saturation: 0.70, // Aiming high
+//					brightness: 1.00, // Aiming high
+//					alpha: 1)
+				return UIColor(named: "grape")!
 			case .strawberry:
 				/*
 				 Hue:
@@ -148,62 +204,6 @@ enum AccentColor: CaseIterable {
 //					brightness: 0.85, // Aiming high
 //					alpha: 1)
 				return UIColor(named: "lime")!
-			case .blueberry:
-				/*
-				 Hue:
-				 • 200 - approaching cyan
-				 • 215 - GOOD. a tinge of yellow
-				 • 235 - approaching purple
-				 
-				 # For hue 215
-				 
-				 Saturation:
-				 • 0.80 - too desaturated
-				 • 0.90 - dark mode: garish at any brightness
-				 
-				 Brightness:
-				 • 0.65 - light mode: annoyingly dark
-				 • 0.90 - dark mode: too dark
-				 */
-//				return UIColor( // Good in light mode, bad in dark mode
-//					hue: 215/360, // Aiming low
-//					saturation: 1.00, // Aiming high
-//					brightness: 0.75, // Aiming low
-//					alpha: 1)
-//				return UIColor( // Good in dark mode
-//					hue: 210/360, // Aiming low
-//					saturation: 0.90, // Aiming high
-//					brightness: 1.00, // Aiming high
-//					alpha: 1)
-				return UIColor(named: "blueberry")!
-			case .grape:
-				/*
-				 Hue:
-				 • 255 - too blue
-				 • 280 - boring lavender
-				 
-				 • 315 - too close to Strawberry
-				 • 325 - approaching maroon
-				 
-				 Saturation:
-				 • 0.60 - too desaturated
-				 • 0.90 - dark mode: too garish
-				 
-				 Brightness:
-				 • 0.60 - light mode: barely different from black
-				 • 0.95 - light mode: annoyingly bright. dark mode: boringly bright
-				 */
-//				return UIColor( // Good in light mode, bad in dark mode
-//					hue: 310/360, // Aiming high
-//					saturation: 1.00, // Aiming high
-//					brightness: 0.65, // Aiming low
-//					alpha: 1)
-//				return UIColor( // Good in dark mode
-//					hue: 310/360, // Aiming high
-//					saturation: 0.70, // Aiming high
-//					brightness: 1.00, // Aiming high
-//					alpha: 1)
-				return UIColor(named: "grape")!
 		}
 	}
 	
