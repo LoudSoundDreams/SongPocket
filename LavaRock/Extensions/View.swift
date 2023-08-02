8d9169ff4bf2bd2a2ed8bb0b5e2756518b72def5
Linus <linus@loudsounddreams.com>
2023-08-02 01:27:33 -0400

    Delete unused code

--- LavaRock/Extensions/View.swift
+++ LavaRock/Extensions/View.swift
@@ -1,19 +1,1 @@
-//
-//  View.swift
-//  LavaRock
-//
-//  Created by h on 2023-03-14.
-//
 
-import SwiftUI
-
-extension View {
-	func backgroundColorRandom() -> some View {
-		background {
-			Color(
-				hue: Double.random(in: 0...1),
-				saturation: Double.random(in: 0...1),
-				brightness: 0.5)
-		}
-	}
-}
