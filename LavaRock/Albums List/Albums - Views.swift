fd54bac4b568fd867b922d48a6d40d401681e4f7
Linus <linus@loudsounddreams.com>
2023-08-11 18:47:44 -0400

    Rearrange code

--- LavaRock/Albums List/Albums - Views.swift
+++ LavaRock/Albums List/Albums - Views.swift
@@ -9,34 +9,6 @@ import UIKit
 import SwiftUI
 import OSLog
 
-struct CoverArtView: View {
-	let albumRepresentative: (any SongInfo)?
-	let largerThanOrEqualToSizeInPoints: CGFloat
-	
-	var body: some View {
-		let uiImageOptional = albumRepresentative?.coverArt(atLeastInPoints: CGSize(
-			width: largerThanOrEqualToSizeInPoints,
-			height: largerThanOrEqualToSizeInPoints))
-		if let uiImage = uiImageOptional {
-			Image(uiImage: uiImage)
-				.resizable() // Lets 1 image point differ from 1 screen point
-				.scaledToFit() // Maintains aspect ratio
-				.accessibilityLabel(LRString.albumArtwork)
-				.accessibilityIgnoresInvertColors()
-		} else {
-			ZStack {
-				Color(uiColor: .secondarySystemBackground) // Close to what Apple Music uses
-					.aspectRatio(1, contentMode: .fit)
-				Image(systemName: "music.note")
-					.foregroundStyle(.secondary)
-					.font(.system(size: .eight * 4))
-			}
-			.accessibilityLabel(LRString.albumArtwork)
-			.accessibilityIgnoresInvertColors()
-		}
-	}
-}
-
 struct AlbumHeader: View {
 	let album: Album
 	let maxHeight: CGFloat
@@ -63,6 +35,34 @@ struct AlbumHeader: View {
 			.padding(.bottom)
 		}
 		.alignmentGuide_separatorLeading()
+	}
+}
+
+struct CoverArtView: View {
+	let albumRepresentative: (any SongInfo)?
+	let largerThanOrEqualToSizeInPoints: CGFloat
+	
+	var body: some View {
+		let uiImageOptional = albumRepresentative?.coverArt(atLeastInPoints: CGSize(
+			width: largerThanOrEqualToSizeInPoints,
+			height: largerThanOrEqualToSizeInPoints))
+		if let uiImage = uiImageOptional {
+			Image(uiImage: uiImage)
+				.resizable() // Lets 1 image point differ from 1 screen point
+				.scaledToFit() // Maintains aspect ratio
+				.accessibilityLabel(LRString.albumArtwork)
+				.accessibilityIgnoresInvertColors()
+		} else {
+			ZStack {
+				Color(uiColor: .secondarySystemBackground) // Close to what Apple Music uses
+					.aspectRatio(1, contentMode: .fit)
+				Image(systemName: "music.note")
+					.foregroundStyle(.secondary)
+					.font(.system(size: .eight * 4))
+			}
+			.accessibilityLabel(LRString.albumArtwork)
+			.accessibilityIgnoresInvertColors()
+		}
 	}
 }
 
