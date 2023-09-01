6bafd48a3277916583cc2af67fa6b3929fe204a9
Linus <linus@loudsounddreams.com>
2023-09-01 10:04:01 -0400

    Delete unnecessary code

--- LavaRock/LavaRock.swift
+++ LavaRock/LavaRock.swift
@@ -31,35 +31,12 @@ struct LavaRock: App {
 		}
 	}
 }
-
 private struct RootNC: UIViewControllerRepresentable {
 	typealias VCType = LibraryNC
 	
-	// Overriding lighting and accent color
-	// We want to do that on the view’s window, but during `makeUIViewController`, that’s nil. So…
-	// • During `make`, override on the view itself. Then, as soon as possible, move the override to the window.
-	// • Thereafter, during `updateUIViewController`, always override on the window.
-	// “As soon as possible” is `viewDidAppear`, because that’s when the window becomes non-nil, and note that that’s after the initial `update`.
-	
-	func makeUIViewController(
-		context: Context
-	) -> VCType {
-		let vc = LibraryNC(rootStoryboardName: "FoldersTVC")
-//		vc.view.tintColor = tint
-		vc.needsOverrideThemeInWindow = true
-		return vc
+	func makeUIViewController(context: Context) -> VCType {
+		return LibraryNC(rootStoryboardName: "FoldersTVC")
 	}
 	
-	// SwiftUI does run this before `VCType.viewDidLoad`.
-	func updateUIViewController(
-		_ uiViewController: VCType,
-		context: Context
-	) {
-//		let vc = uiViewController
-//		let window = vc.view.window
-		
-		// Unfortunately, we can’t remove a view’s tint color override.
-		// So, override the tint color on both the view and its window, every time.
-//		vc.view.tintColor = tint
-	}
+	func updateUIViewController(_ uiViewController: VCType, context: Context) {}
 }
--- LavaRock/Library/LibraryNC.swift
+++ LavaRock/Library/LibraryNC.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class LibraryNC: UINavigationController {
-	var needsOverrideThemeInWindow = false
-	
 	lazy var mainToolbar = MainToolbar__UIKit()
 	
 	init(rootStoryboardName: String) {
@@ -27,20 +25,5 @@ final class LibraryNC: UINavigationController {
 		super.viewDidLoad()
 		
 		toolbar.scrollEdgeAppearance = toolbar.standardAppearance
-	}
-	
-	// Xcode 15: Move this to `viewIsAppearing`? (Back-deployed to iOS 13)
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		if needsOverrideThemeInWindow {
-			needsOverrideThemeInWindow = false
-			
-//			let window = view.window!
-			
-			// Accent color
-//			window.tintColor = view.tintColor
-			// Unfortunately, setting `view.tintColor = nil` doesn’t remove the override.
-		}
 	}
 }
