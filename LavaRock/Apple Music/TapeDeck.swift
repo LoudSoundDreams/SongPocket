07c196ef13a568af9692b06b5d6e9e507a0a8696
Linus <linus@loudsounddreams.com>
2023-11-17 08:43:54 -0500

    Rearrange code

--- LavaRock/Apple Music/TapeDeck.swift
+++ LavaRock/Apple Music/TapeDeck.swift
@@ -5,6 +5,18 @@
 //  Created by h on 2020-11-04.
 //
 
+@objc // “generic class 'Weak' requires that 'TapeDeckReflecting' be a class type”
+@MainActor
+protocol TapeDeckReflecting: AnyObject {
+	// Adopting types must…
+	// • Call `TapeDeck.shared.addReflector(weakly: self)` as soon as their implementations of `reflect_playbackState` and `reflect_nowPlaying` will work.
+	
+	func reflect_playbackState()
+	// Reflect `TapeDeck.shared.player`, and show a disabled state if it’s `nil`. (Call `TapeDeck.shared.beginWatching` to set it up.)
+	
+	func reflect_nowPlaying()
+}
+
 import MediaPlayer
 
 @MainActor
