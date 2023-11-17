83905a0bb9ba44e94bef287a0f2dc003141e946b
Linus <linus@loudsounddreams.com>
2023-11-17 09:32:56 -0500

    Rearrange code

--- LavaRock/Persistence/Persistence.swift
+++ LavaRock/Persistence/Persistence.swift
@@ -1,12 +1,57 @@
 //
-//  Database.swift
+//  Persistence.swift
 //  LavaRock
 //
 //  Created by h on 2021-12-30.
 //
 
-import CoreData
+// Keeping these keys in one place helps us keep them unique.
+enum LRDefaultsKey: String, CaseIterable {
+	// Introduced in version ?
+	case hasSavedDatabase = "hasEverImportedFromMusic"
+	
+	/*
+	 Deprecated after version 1.13.3
+	 Introduced in version 1.8
+	 "nowPlayingIcon"
+	 Values: String
+	 Introduced in version 1.12
+	 • "Paw"
+	 • "Luxo"
+	 Introduced in version 1.8
+	 • "Speaker"
+	 • "Fish"
+	 Deprecated after version 1.11.2:
+	 • "Bird"
+	 • "Sailboat"
+	 • "Beach umbrella"
+	 
+	 Deprecated after version 1.13.3
+	 Introduced in version 1.0
+	 "accentColorName"
+	 Values: String
+	 • "Blueberry"
+	 • "Grape"
+	 • "Strawberry"
+	 • "Tangerine"
+	 • "Lime"
+	 
+	 Deprecated after version 1.13
+	 Introduced in version 1.6
+	 "appearance"
+	 Values: Int
+	 • `0` for “match system”
+	 • `1` for “always light”
+	 • `2` for “always dark”
+	 
+	 Deprecated after version 1.7
+	 Introduced in version ?
+	 "shouldExplainQueueAction"
+	 Values: Bool
+	 */
+}
 
+import CoreData
 struct Database {
 	private init() {}
 	
