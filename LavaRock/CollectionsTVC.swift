d061794cbd8fb7e8fef922ab5b42baf3c33aea58
Linus <linus@loudsounddreams.com>
2024-03-11 12:49:55 -0700

    Rearrange code

--- LavaRock/CollectionsTVC.swift
+++ LavaRock/CollectionsTVC.swift
@@ -331,4 +331,112 @@ final class CollectionsTVC: LibraryTVC {
 			collection: selectedCollection,
 			context: viewModel.context)
 	}
+	
+	// MARK: - Table view
+	
+	override func tableView(
+		_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath
+	) {
+		promptRename(at: indexPath)
+	}
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		switch viewState {
+			case .someCollections:
+				contentUnavailableConfiguration = nil
+			case .allowAccess:
+				contentUnavailableConfiguration = UIHostingConfiguration {
+					ContentUnavailableView {
+					} description: {
+						Text(LRString.welcome_message)
+					} actions: {
+						Button {
+							Task {
+								await self.requestAccessToAppleMusic()
+							}
+						} label: {
+							Text(LRString.welcome_button)
+						}
+					}
+				}
+			case .loading:
+				contentUnavailableConfiguration = UIHostingConfiguration {
+					ProgressView().tint(.secondary)
+				}
+			case .emptyDatabase:
+				contentUnavailableConfiguration = UIHostingConfiguration {
+					ContentUnavailableView {
+					} actions: {
+						Button {
+							let musicURL = URL(string: "music://")!
+							UIApplication.shared.open(musicURL)
+						} label: {
+							Text(LRString.emptyLibrary_button)
+						}
+					}
+				}
+		}
+		
+		return 1
+	}
+	
+	override func tableView(
+		_ tableView: UITableView, numberOfRowsInSection section: Int
+	)-> Int {
+		switch viewState {
+			case .allowAccess, .loading, .emptyDatabase: return 0
+			case .someCollections:
+				return viewModel.libraryGroup().items.count
+		}
+	}
+	
+	override func tableView(
+		_ tableView: UITableView, cellForRowAt indexPath: IndexPath
+	) -> UITableViewCell {
+		// The cell in the storyboard is completely default except for the reuse identifier and selection segue.
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Folder", for: indexPath)
+		let collectionsViewModel = viewModel as! CollectionsViewModel
+		let collection = collectionsViewModel.collectionNonNil(atRow: indexPath.row)
+		let enabled = { () -> Bool in
+			switch purpose {
+				case .movingAlbums(let clipboard):
+					if clipboard.idsOfSourceCollections.contains(collection.objectID) {
+						return false
+					}
+					return true
+				case .browsing: return true
+			}
+		}()
+		cell.contentConfiguration = UIHostingConfiguration {
+			CollectionRow(title: collection.title, collection: collection, dimmed: !enabled)
+		}
+		cell.editingAccessoryType = .detailButton
+		cell.backgroundColors_configureForLibraryItem()
+		cell.isUserInteractionEnabled = enabled
+		if enabled {
+			cell.accessibilityTraits.subtract(.notEnabled)
+		} else {
+			cell.accessibilityTraits.formUnion(.notEnabled)
+		}
+		switch purpose {
+			case .movingAlbums: break
+			case .browsing:
+				cell.accessibilityCustomActions = [
+					UIAccessibilityCustomAction(name: LRString.rename) { [weak self] action in
+						guard
+							let self,
+							let focused = tableView.allIndexPaths().first(where: {
+								let cell = tableView.cellForRow(at: $0)
+								return cell?.accessibilityElementIsFocused() ?? false
+							})
+						else {
+							return false
+						}
+						promptRename(at: focused)
+						return true
+					}
+				]
+		}
+		return cell
+	}
 }
--- LavaRock/LibraryTVC.swift
+++ LavaRock/LibraryTVC.swift
@@ -328,4 +328,75 @@ class LibraryTVC: UITableViewController {
 			let _ = await setViewModelAndMoveAndDeselectRowsAndShouldContinue(newViewModel)
 		}
 	}
+	
+	// MARK: - Table view
+	
+	final override func tableView(
+		_ tableView: UITableView, canEditRowAt indexPath: IndexPath
+	) -> Bool {
+		return viewModel.pointsToSomeItem(row: indexPath.row)
+	}
+	
+	final override func tableView(
+		_ tableView: UITableView,
+		targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath,
+		toProposedIndexPath proposedDestinationIndexPath: IndexPath
+	) -> IndexPath {
+		if viewModel.pointsToSomeItem(row: proposedDestinationIndexPath.row) {
+			return proposedDestinationIndexPath
+		}
+		
+		// Reordering upward
+		if proposedDestinationIndexPath < sourceIndexPath {
+			return IndexPath(
+				row: viewModel.row(forItemIndex: 0),
+				section: proposedDestinationIndexPath.section)
+		}
+		
+		// Reordering downward
+		return proposedDestinationIndexPath
+	}
+	
+	final override func tableView(
+		_ tableView: UITableView,
+		moveRowAt fromIndexPath: IndexPath,
+		to: IndexPath
+	) {
+		let fromIndex = viewModel.itemIndex(forRow: fromIndexPath.row)
+		let toIndex = viewModel.itemIndex(forRow: to.row)
+		
+		var newItems = viewModel.libraryGroup().items
+		let itemBeingMoved = newItems.remove(at: fromIndex)
+		newItems.insert(itemBeingMoved, at: toIndex)
+		viewModel.groups[0].items = newItems
+		
+		freshenEditingButtons() // If you made selected rows non-contiguous, that should disable the “Sort” button. If you made all the selected rows contiguous, that should enable the “Sort” button.
+	}
+	
+	final override func tableView(
+		_ tableView: UITableView, willSelectRowAt indexPath: IndexPath
+	) -> IndexPath? {
+		return viewModel.pointsToSomeItem(row: indexPath.row) ? indexPath : nil
+	}
+	
+	// Overrides should call super (this implementation) if `viewModel.pointsToSomeItem(indexPath)`.
+	override func tableView(
+		_ tableView: UITableView, didSelectRowAt indexPath: IndexPath
+	) {
+		if isEditing {
+			if let cell = tableView.cellForRow(at: indexPath) {
+				cell.accessibilityTraits.formUnion(.selected)
+			}
+			freshenEditingButtons()
+		}
+	}
+	
+	final override func tableView(
+		_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath
+	) {
+		if let cell = tableView.cellForRow(at: indexPath) {
+			cell.accessibilityTraits.subtract(.selected)
+		}
+		freshenEditingButtons()
+	}
 }
