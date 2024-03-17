d62f7edd558dd8e3d8ddee88d944808f03f42e28
Linus <linus@loudsounddreams.com>
2024-03-18 04:48:29 +0800

    Rearrange code

--- LavaRock/Apple Music.swift
+++ LavaRock/Apple Music.swift
@@ -21,10 +21,43 @@ import MusicKit
 		return .shared
 	}
 }
+
 import MediaPlayer
 extension MPMusicPlayerController {
 	static var _system: MPMusicPlayerController? {
 		guard MPMediaLibrary.authorizationStatus() == .authorized else { return nil }
 		return .systemMusicPlayer
+	}
+}
+@MainActor final class AudioPlayer {
+	private init() {}
+	static let shared = AudioPlayer()
+	
+	var reflectorToolbar: Weak<MainToolbar>? = nil
+	
+	func watchMPPlayer() {
+		guard let __player = MPMusicPlayerController._system else { return }
+		
+		__player.beginGeneratingPlaybackNotifications()
+		
+		playbackState() // Because before anyone called `watchMPPlayer`, `player` was `nil`, and `MPMediaLibrary.authorizationStatus` might not have been `.authorized`.
+		nowPlaying()
+		
+		NotificationCenter.default.addObserverOnce(
+			self,
+			selector: #selector(playbackState),
+			name: .MPMusicPlayerControllerPlaybackStateDidChange, // As of iOS 15.4, Media Player also posts this when the repeat or shuffle mode changes.
+			object: nil)
+		NotificationCenter.default.addObserverOnce(
+			self,
+			selector: #selector(nowPlaying),
+			name: .MPMusicPlayerControllerNowPlayingItemDidChange,
+			object: nil)
+	}
+	@objc private func playbackState() {
+		reflectorToolbar?.referencee?.freshen()
+	}
+	@objc private func nowPlaying() {
+		reflectorToolbar?.referencee?.freshen()
 	}
 }
