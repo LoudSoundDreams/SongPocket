a3faf8a68384ababdf6c593d76786b46146106a9
Linus <linus@loudsounddreams.com>
2024-03-18 03:25:05 +0800

    Rearrange code

--- LavaRock/MainToolbar.swift
+++ LavaRock/MainToolbar.swift
@@ -249,3 +249,59 @@ extension MainToolbar: TapeDeckReflecting {
 	func reflect_playbackState() { freshen() }
 	func reflect_nowPlaying() { freshen() }
 }
+
+// MARK: - SwiftUI
+
+import SwiftUI
+
+struct MainToolbar_Previews: PreviewProvider {
+	static var previews: some View {
+		Color.clear.mainToolbar()
+	}
+}
+extension View {
+	func mainToolbar() -> some View {
+		toolbar {
+			ToolbarItem(placement: .bottomBar) { selectButton }
+			ToolbarItem(placement: .bottomBar) { Spacer() }
+			ToolbarItem(placement: .bottomBar) { jumpBackButton }
+			ToolbarItem(placement: .bottomBar) { Spacer() }
+			ToolbarItem(placement: .bottomBar) { playPauseButton }
+			ToolbarItem(placement: .bottomBar) { Spacer() }
+			ToolbarItem(placement: .bottomBar) { jumpForwardButton }
+			ToolbarItem(placement: .bottomBar) { Spacer() }
+			ToolbarItem(placement: .bottomBar) { overflowButton }
+		}
+	}
+	
+	private var selectButton: some View {
+		Button {
+		} label: {
+			Image(systemName: "checkmark.circle")
+		}
+	}
+	private var jumpBackButton: some View {
+		Button {
+		} label: {
+			Image(systemName: "gobackward.15")
+		}
+	}
+	private var playPauseButton: some View {
+		Button {
+		} label: {
+			Image(systemName: "play.circle")
+		}
+	}
+	private var jumpForwardButton: some View {
+		Button {
+		} label: {
+			Image(systemName: "goforward.15")
+		}
+	}
+	private var overflowButton: some View {
+		Menu {
+		} label: {
+			Image(systemName: "ellipsis.circle")
+		}
+	}
+}
