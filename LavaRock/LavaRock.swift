801cec3424257b9b8b72ee748b5b65202d45667a
Linus <linus@loudsounddreams.com>
2024-03-31 22:58:15 +0800

    Rearrange code

--- LavaRock/LavaRock.swift
+++ LavaRock/LavaRock.swift
@@ -75,3 +75,49 @@ private final class RootNC: UINavigationController {
 	}
 	private static var hasAppeared = false
 }
+
+// Keeping these keys in one place helps us keep them unique.
+enum LRDefaultsKey: String, CaseIterable {
+	// Introduced in version ?
+	case hasSavedDatabase = "hasEverImportedFromMusic"
+	
+	/*
+	 Deprecated after version 1.13.3
+	 Introduced in version 1.8
+	 "nowPlayingIcon"
+	 Values: String
+	 Introduced in version 1.12
+	 • "Paw"
+	 • "Luxo"
+	 Introduced in version 1.8
+	 • "Speaker"
+	 • "Fish"
+	 Deprecated after version 1.11.2:
+	 • "Bird"
+	 • "Sailboat"
+	 • "Beach umbrella"
+	 
+	 Deprecated after version 1.13.3
+	 Introduced in version 1.0
+	 "accentColorName"
+	 Values: String
+	 • "Blueberry"
+	 • "Grape"
+	 • "Strawberry"
+	 • "Tangerine"
+	 • "Lime"
+	 
+	 Deprecated after version 1.13
+	 Introduced in version 1.6
+	 "appearance"
+	 Values: Int
+	 • `0` for “match system”
+	 • `1` for “always light”
+	 • `2` for “always dark”
+	 
+	 Deprecated after version 1.7
+	 Introduced in version ?
+	 "shouldExplainQueueAction"
+	 Values: Bool
+	 */
+}
