3b5e06eebde296a71d57aac2c08f3aaefbb9b66b
Linus <linus@loudsounddreams.com>
2024-04-05 22:28:21 +0800

    Rearrange code

--- LavaRock/LibraryViewModel.swift
+++ LavaRock/LibraryViewModel.swift
@@ -18,3 +18,84 @@ extension LibraryViewModel {
 		return items[itemIndex]
 	}
 }
+
+struct AlbumsViewModel {
+	// `LibraryViewModel`
+	var items: [NSManagedObject] {
+		didSet { Library.renumber(items) }
+	}
+}
+extension AlbumsViewModel: LibraryViewModel {
+	func itemIndex(forRow row: Int) -> Int { return row }
+	func rowsForAllItems() -> [Int] {
+		return items.indices.map { $0 }
+	}
+	func row(forItemIndex itemIndex: Int) -> Int { return itemIndex }
+	
+	// Similar to counterpart in `SongsViewModel`.
+	func updatedWithFreshenedData() -> Self {
+		return Self()
+	}
+	
+	func rowIdentifiers() -> [AnyHashable] {
+		return items.map { $0.objectID }
+	}
+}
+extension AlbumsViewModel {
+	init() {
+		if
+			let collection = Collection.allFetched(sorted: false, context: Database.viewContext).first,
+			let context = collection.managedObjectContext
+		{
+			items = Album.allFetched(sorted: true, inCollection: collection, context: context)
+		} else {
+			// We deleted `collection`
+			items = []
+		}
+	}
+}
+
+struct SongsViewModel {
+	static let prerowCount = 1
+	let album: Album
+	
+	// `LibraryViewModel`
+	var items: [NSManagedObject] {
+		didSet { Library.renumber(items) }
+	}
+}
+extension SongsViewModel: LibraryViewModel {
+	func itemIndex(forRow row: Int) -> Int {
+		return row - Self.prerowCount
+	}
+	func rowsForAllItems() -> [Int] {
+		return items.indices.map {
+			Self.prerowCount + $0
+		}
+	}
+	func row(forItemIndex itemIndex: Int) -> Int {
+		return Self.prerowCount + itemIndex
+	}
+	
+	// Similar to counterpart in `AlbumsViewModel`.
+	func updatedWithFreshenedData() -> Self {
+		return Self(album: album)
+	}
+	
+	func rowIdentifiers() -> [AnyHashable] {
+		let itemRowIDs = items.map {
+			AnyHashable($0.objectID)
+		}
+		return [42] + itemRowIDs
+	}
+}
+extension SongsViewModel {
+	init(album: Album) {
+		if let context = album.managedObjectContext {
+			items = Song.allFetched(sorted: true, inAlbum: album, context: context)
+		} else {
+			items = []
+		}
+		self.album = album
+	}
+}
