9dd78736b20392c205b19d96bb39bd9143905516
Linus <linus@loudsounddreams.com>
2024-04-10 08:45:54 +0800

    Renumber fake albums

--- LavaRock/Album View.swift
+++ LavaRock/Album View.swift
@@ -4,10 +4,9 @@ import SwiftUI
 
 struct AlbumShelf: View {
 	@State private var albums: [FakeAlbum] = FakeAlbum.demoArray {
-		didSet {
-		}
+		didSet { FakeAlbum.renumber(albums) }
 	}
-	@State private var visiblePosition: Int? = 2
+	@State private var visibleIndex: Int? = 2
 	var body: some View {
 		ScrollViewReader { proxy in
 			ScrollView(.horizontal) {
@@ -20,12 +19,13 @@ struct AlbumShelf: View {
 						}
 						.aspectRatio(1, contentMode: .fit)
 						.containerRelativeFrame(.horizontal)
+						.id(album.position)
 					}
 				}
 				.scrollTargetLayout()
 			}
 			.scrollTargetBehavior(.viewAligned(limitBehavior: .never))
-			.scrollPosition(id: $visiblePosition)
+			.scrollPosition(id: $visibleIndex)
 			.toolbar(.visible, for: .bottomBar)
 			.toolbarBackground(.visible, for: .bottomBar)
 			.toolbar { ToolbarItemGroup(placement: .bottomBar) {
@@ -34,23 +34,23 @@ struct AlbumShelf: View {
 				Spacer()
 				Button {
 					withAnimation {
-						if let pos = visiblePosition, pos > 0 { visiblePosition = pos - 1 }
+						if let pos = visibleIndex, pos > 0 { visibleIndex = pos - 1 }
 					}
 				} label: { Image(systemName: "arrow.backward") }
-					.disabled({ guard let visiblePosition else { return false }; return visiblePosition <= 0 }())
+					.disabled({ guard let visibleIndex else { return false }; return visibleIndex <= 0 }())
 				Spacer()
 				ZStack {
 					Text(String(albums.count - 1)).hidden()
-					Text({ guard let visiblePosition else { return "" }; return String(visiblePosition) }())
+					Text({ guard let visibleIndex else { return "" }; return String(visibleIndex) }())
 						.animation(.none)
 				}
 				Spacer()
 				Button {
 					withAnimation {
-						if let pos = visiblePosition, pos < albums.count - 1 { visiblePosition = pos + 1 }
+						if let pos = visibleIndex, pos < albums.count - 1 { visibleIndex = pos + 1 }
 					}
 				} label: { Image(systemName: "arrow.forward") }
-					.disabled({ guard let visiblePosition else { return false }; return visiblePosition >= albums.count - 1 }())
+					.disabled({ guard let visibleIndex else { return false }; return visibleIndex >= albums.count - 1 }())
 				Spacer()
 				Button {
 					albums.append(FakeAlbum(position: albums.count, title: .randomLowercaseLetter()))
@@ -62,8 +62,7 @@ struct AlbumShelf: View {
 
 struct AlbumList: View {
 	@State private var albums: [FakeAlbum] = FakeAlbum.demoArray {
-		didSet {
-		}
+		didSet { FakeAlbum.renumber(albums) }
 	}
 	@State private var selectedAlbums: Set<FakeAlbum> = []
 	var body: some View {
@@ -78,17 +77,37 @@ struct AlbumList: View {
 	}
 }
 
-struct FakeAlbum: Hashable {
-	static let demoArray: [Self] = {
-		return (0...3).map { Self(position: $0, title: .randomLowercaseLetter()) }
+// If this were a struct, `[FakeAlbum].didSet` would loop infinitely when you set one of `FakeAlbum`’s properties.
+final class FakeAlbum: Identifiable {
+	static let demoArray: [FakeAlbum] = {
+		return (0...3).map { FakeAlbum(position: $0, title: .randomLowercaseLetter()) }
 	}()
+	static func renumber(_ albums: [FakeAlbum]) {
+		albums.indices.forEach { albums[$0].position = $0 }
+	}
+	
 	var position: Int
 	let title: String
 	let circleColor = Color.random()
 	let squareColor = Color.random()
+	
+	init(position: Int, title: String) {
+		self.position = position
+		self.title = title
+	}
+	
+	// `Identifiable`
+	let id = UUID()
 }
-extension FakeAlbum: Identifiable {
-	var id: Int { position }
+extension FakeAlbum: Equatable {
+	static func == (lhs: FakeAlbum, rhs: FakeAlbum) -> Bool {
+		return lhs.position == rhs.position
+	}
+}
+extension FakeAlbum: Hashable {
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(position)
+	}
 }
 
 extension String {
