b9a51ad4be913a8016461af435b918e645fddb27
Linus <linus@loudsounddreams.com>
2024-04-18 13:11:08 -0700

    Neaten code

--- LavaRock/Library Views.swift
+++ LavaRock/Library Views.swift
@@ -7,33 +7,18 @@ struct AvatarImage: View {
 	let libraryItem: LibraryItem
 	@ObservedObject var state: MusicPlayer.State
 	@ObservedObject var queue: MusicPlayer.Queue
-	
-	private var status: Status {
-		guard libraryItem.containsPlayhead() else { return .notPlaying }
-#if targetEnvironment(simulator)
-		return .playing
-#else
-		return (state.playbackStatus == .playing) ? .playing : .paused
-#endif
-	}
-	enum Status {
-		case notPlaying, paused, playing
-	}
 	@ObservedObject private var musicRepo: MusicRepo = .shared // In case the user added or deleted the current song. Currently, even if the view body never actually mentions this, merely including this property refreshes the view at the right times.
-	
 	var body: some View {
 		ZStack(alignment: .leading) {
 			AvatarPlayingImage().hidden()
 			foregroundView
 		}
 		.accessibilityElement()
-		.accessibilityLabel({
-			switch status {
-				case .notPlaying: return ""
-				case .paused: return LRString.paused
-				case .playing: return LRString.nowPlaying
-			}
-		}())
+		.accessibilityLabel({ switch status {
+			case .notPlaying: return ""
+			case .paused: return LRString.paused
+			case .playing: return LRString.nowPlaying
+		}}())
 	}
 	@ViewBuilder private var foregroundView: some View {
 		switch status {
@@ -49,6 +34,15 @@ struct AvatarImage: View {
 					.symbolRenderingMode(.hierarchical)
 		}
 	}
+	private var status: Status {
+		guard libraryItem.containsPlayhead() else { return .notPlaying }
+#if targetEnvironment(simulator)
+		return .playing
+#else
+		return (state.playbackStatus == .playing) ? .playing : .paused
+#endif
+	}
+	enum Status { case notPlaying, paused, playing }
 }
 struct AvatarPlayingImage: View {
 	var body: some View {
@@ -63,7 +57,6 @@ struct AvatarPlayingImage: View {
 struct AlbumRow: View {
 	let album: Album
 	let maxHeight: CGFloat
-	
 	var body: some View {
 		VStack(spacing: 0) {
 			Rectangle().frame(height: 1/2 * Self.borderWidthInPixels * pointsPerPixel).hidden()
@@ -100,7 +93,6 @@ struct AlbumRow: View {
 private struct CoverArt: View {
 	let album: Album
 	let largerThanOrEqualToSizeInPoints: CGFloat
-	
 	var body: some View {
 		let uiImageOptional = album.representativeSongInfo()?.coverArt(atLeastInPoints: CGSize(width: largerThanOrEqualToSizeInPoints, height: largerThanOrEqualToSizeInPoints))
 		if let uiImage = uiImageOptional {
@@ -123,7 +115,6 @@ private struct CoverArt: View {
 }
 private struct AlbumLabel: View {
 	let album: Album
-	
 	var body: some View {
 		HStack(alignment: .firstTextBaseline) {
 			ZStack(alignment: .leading) {
@@ -157,7 +148,6 @@ struct Chevron: View {
 
 struct AlbumHeader: View {
 	let album: Album
-	
 	var body: some View {
 		HStack(spacing: .eight * 5/4) {
 			AvatarPlayingImage().hidden()
@@ -187,7 +177,6 @@ struct SongRow: View {
 	let trackDisplay: String
 	let artist_if_different_from_album_artist: String?
 	@ObservedObject var listStatus: SongsListStatus
-	
 	var body: some View {
 		HStack(alignment: .firstTextBaseline) {
 			HStack(alignment: .firstTextBaseline) {
