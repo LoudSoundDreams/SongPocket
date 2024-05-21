c3c5878e45517c1c447bf70bbb497f873ba9dab4
Linus <linus@loudsounddreams.com>
2024-05-21 10:23:47 -0700

    Rewrite code for clarity

--- LavaRock/MusicRepo.swift
+++ LavaRock/MusicRepo.swift
@@ -39,43 +39,50 @@ final class MusicRepo: ObservableObject {
 		
 		// Find out which existing `Song`s we need to delete, and which we need to potentially update.
 		// Meanwhile, isolate the `SongInfo`s that we don’t have `Song`s for. We’ll create new `Song`s for them.
-		var maybeOutdated: [(existing: Song, fresh: SongInfo)] = [] // We’ll sort these eventually.
-		var toDelete: [Song] = []
-		
-		var infosBySongID: Dictionary<SongID, SongInfo> = {
-			let tuples = freshInAnyOrder.map { info in (info.songID, info) }
-			return Dictionary(uniqueKeysWithValues: tuples)
-		}()
-		
-		let existingSongs: [Song] = context.objectsFetched(for: Song.fetchRequest()) // Not sorted
-		existingSongs.forEach { existingSong in
-			let songID = existingSong.persistentID
-			if let maybeUpdatedInfo = infosBySongID[songID] {
-				// We have an existing `Song` for this `SongInfo`. We might need to update it.
-				maybeOutdated.append((existing: existingSong, fresh: maybeUpdatedInfo))
-				
-				infosBySongID[songID] = nil
-			} else {
-				// This `Song` no longer corresponds to any `SongInfo`. We’ll delete it.
-				toDelete.append(existingSong)
+		let toUpdate: [(existing: Song, fresh: SongInfo)]
+		let toDelete: [Song]
+		let toCreate: [SongInfo]
+		do {
+			var updates: [(Song, SongInfo)] = []
+			var deletes: [Song] = []
+			
+			var freshInfos: [SongID: SongInfo] = {
+				let tuples = freshInAnyOrder.map { info in (info.songID, info) }
+				return Dictionary(uniqueKeysWithValues: tuples)
+			}()
+			let existingSongs: [Song] = context.objectsFetched(for: Song.fetchRequest()) // Not sorted
+			existingSongs.forEach { existingSong in
+				let songID = existingSong.persistentID
+				if let freshInfo = freshInfos[songID] {
+					// We have an existing `Song` for this `SongInfo`. We might need to update the `Song`.
+					updates.append((existingSong, freshInfo)) // We’ll sort these later.
+					
+					freshInfos[songID] = nil
+				} else {
+					// This `Song` no longer corresponds to any `SongInfo` in the library. We’ll delete it.
+					deletes.append(existingSong)
+				}
 			}
+			// `freshInfos` now holds the `SongInfo`s that we don’t have `Song`s for.
+			
+			toUpdate = updates
+			toDelete = deletes
+			toCreate = freshInfos.map { $0.value } // We’ll sort these later.
 		}
-		// `infosBySongID` now holds the `SongInfo`s that we don’t have `Song`s for.
-		let newInfos: [SongInfo] = infosBySongID.map { $0.value }
 		
 		// Update before creating and deleting, so that we can easily put new `Song`s above modified `Song`s.
 		// This also deletes all but one `Album` with any given `albumPersistentID`.
 		// This might create `Album`s, but not `Collection`s or `Song`s.
 		// This might delete `Album`s, but not `Collection`s or `Song`s.
 		// This also might leave behind empty `Album`s. We don’t delete those here, so that if the user also added other `Song`s to those `Album`s, we can keep those `Album`s in the same place, instead of re-adding them to the top.
-		updateLibraryItems(existingAndFresh: maybeOutdated)
+		updateLibraryItems(existingAndFresh: toUpdate)
 		
 		// Create before deleting, because deleting also cleans up empty `Album`s and `Collection`s, which we shouldn’t do yet (see above).
 		// This might create new `Album`s, and if it does, it might create new `Collection`s.
 		let existingAlbums = Album.allFetched(sorted: false, context: context) // Order doesn’t matter, because we identify `Album`s by their `albumPersistentID`.
 		let existingCollections = Collection.allFetched(sorted: true, context: context) // Order matters, because we’ll try to add new `Album`s to the first `Collection` with a matching title.
 		createLibraryItems(
-			for: newInfos,
+			for: toCreate,
 			existingAlbums: existingAlbums,
 			existingCollections: existingCollections,
 			isFirstImport: isFirstImport)
