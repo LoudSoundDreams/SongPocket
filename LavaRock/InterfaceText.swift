1d57ab227251c68d2de65ad154d456dbb5c0dddf
Linus <linus@loudsounddreams.com>
2024-09-13 12:33:57 -0700

    Rearrange code

--- LavaRock/InterfaceText.swift
+++ LavaRock/InterfaceText.swift
@@ -6,6 +6,8 @@ enum InterfaceText {
 	
 	// MARK: - WITHOUT VARIABLES
 	
+	// MARK: General
+	
 	static let cancel = NSLocalizedString("Cancel", comment: "Button")
 	static let done = NSLocalizedString("Done", comment: "Button")
 	static let more = NSLocalizedString("More", comment: "Button")
@@ -17,36 +19,36 @@ enum InterfaceText {
 	
 	static let unknownArtist = NSLocalizedString("Unknown Artist", comment: "Stand-in for missing info on albums and songs")
 	static let unknownAlbum = NSLocalizedString("Unknown Album", comment: "Stand-in for missing album title")
+	static let albumArtwork = NSLocalizedString("Album artwork", comment: "Accessibility label")
+	static let nowPlaying = NSLocalizedString("Now Playing", comment: "Button and accessibility label")
+	static let paused = NSLocalizedString("Paused", comment: "Accessibility label")
+	static let appleMusic = NSLocalizedString("Apple Music", comment: "Button")
 	
-	// MARK: Toolbar
-	
+	static let welcome_message = NSLocalizedString("SongPocket shows and plays your Apple Music library.", comment: "Placeholder when no access to Apple Music; subtitle")
+	static let welcome_button = NSLocalizedString("Continue", comment: "Button")
 	static let _emptyLibraryMessage = NSLocalizedString("Add music to your Apple Music library.", comment: "Message in menu when database is empty")
 	
-	static let nowPlaying = NSLocalizedString("Now Playing", comment: "Button and accessibility label")
-	static let repeat1 = NSLocalizedString("Repeat One", comment: "Button")
-	static let appleMusic = NSLocalizedString("Apple Music", comment: "Button")
+	// MARK: Playback
+	
+	static let startPlaying = NSLocalizedString("Start Playing", comment: "Button")
+	static let addToQueue = NSLocalizedString("Add to Queue", comment: "Button")
+	static let addRestOfAlbumToQueue = NSLocalizedString("Add Rest of Album to Queue", comment: "Button")
+	static let playNext = NSLocalizedString("Play Next", comment: "Button")
+	static let playRestOfAlbumNext = NSLocalizedString("Play Rest of Album Next", comment: "Button")
+	
+	static let play = NSLocalizedString("Play", comment: "Button")
+	static let pause = NSLocalizedString("Pause", comment: "Button")
+	static let skipBack15Seconds = NSLocalizedString("Skip back 15 seconds", comment: "Button") // As of iOS 16.5 RC 1, picture-in-picture videos use “Skip back 10 seconds” and “Skip forward 10 seconds”.
+	static let skipForward15Seconds = NSLocalizedString("Skip forward 15 seconds", comment: "Button")
 	
 	static let previous = NSLocalizedString("Previous", comment: "Button")
 	static let restart = NSLocalizedString("Restart", comment: "Button")
 	static let next = NSLocalizedString("Next", comment: "Button")
 	
-	static let skipBack15Seconds = NSLocalizedString("Skip back 15 seconds", comment: "Button") // As of iOS 16.5 RC 1, picture-in-picture videos use “Skip back 10 seconds” and “Skip forward 10 seconds”.
-	static let play = NSLocalizedString("Play", comment: "Button")
-	static let pause = NSLocalizedString("Pause", comment: "Button")
-	static let skipForward15Seconds = NSLocalizedString("Skip forward 15 seconds", comment: "Button")
-	
 	static let shuffle = NSLocalizedString("Shuffle", comment: "Button")
-	static let addToQueue = NSLocalizedString("Add to Queue", comment: "Button")
+	static let repeat1 = NSLocalizedString("Repeat One", comment: "Button")
 	
-	// MARK: Library
-	
-	static let welcome_message = NSLocalizedString("SongPocket shows and plays your Apple Music library.", comment: "Placeholder when no access to Apple Music; subtitle")
-	static let welcome_button = NSLocalizedString("Continue", comment: "Button")
-	static let albumArtwork = NSLocalizedString("Album artwork", comment: "Accessibility label")
-	
-	static let startPlaying = NSLocalizedString("Start Playing", comment: "Button")
-	static let paused = NSLocalizedString("Paused", comment: "Accessibility label")
-	static let addRestOfAlbumToQueue = NSLocalizedString("Add Rest of Album to Queue", comment: "Button")
+	// MARK: Editing
 	
 	static let select = NSLocalizedString("Select", comment: "Button")
 	static let selected = NSLocalizedString("Selected", comment: "Accessibility label")
@@ -54,8 +56,6 @@ enum InterfaceText {
 	static let selectDown = NSLocalizedString("Select Down", comment: "Button")
 	static let deselectUp = NSLocalizedString("Deselect Up", comment: "Button")
 	static let deselectDown = NSLocalizedString("Deselect Down", comment: "Button")
-	
-	// MARK: Editing
 	
 	static let sort = NSLocalizedString("Sort", comment: "Button")
 	static let recentlyAdded = NSLocalizedString("Recently Added", comment: "Sort option")
