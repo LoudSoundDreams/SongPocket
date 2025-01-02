525bd533a0263e37904154b4ac1e7ea99b8d7093
Linus <linus@loudsounddreams.com>
2025-01-02 13:39:14 -0800

    Improve page view

--- LavaRock/App.swift
+++ LavaRock/App.swift
@@ -61,10 +61,39 @@ enum WorkingOn {
 
 private struct AlbumGallery: View {
 	var body: some View {
-		TabView { ForEach(0 ..< 10) { _ in
-			RepNCAlbumTable().containerRelativeFrame(.horizontal)
-		}}.tabViewStyle(.page(indexDisplayMode: .never))
+		TabView(selection: $rando_spotlighted) {
+			ForEach(randos, id: \.self) { rando in
+				Text("\(rando)").frame(width: .eight * 32)
+					.onTapGesture {
+						guard let i_rando = randos.firstIndex(of: rando) else { return }
+						let new_rando_spotlighted: UUID? = {
+							guard randos.count >= 2 else { return nil }
+							let i_next_rando: Int = min(i_rando + 1, randos.count - 1)
+							let result = randos[i_next_rando]
+							return result
+						}()
+						let _ = withAnimation { // Unreliable; the first time after any swipe, SwiftUI crossfades the new tab into place rather than pushing it.
+							randos.remove(at: i_rando)
+							if let new_rando_spotlighted {
+								rando_spotlighted = new_rando_spotlighted
+							}
+						}
+					}
+				
+//				RepNCAlbumTable().containerRelativeFrame(.horizontal)
+			}
+		}.tabViewStyle(.page(indexDisplayMode: .never))
 	}
+	@State private var randos: [UUID] = {
+		var result: [UUID] = []
+		result.append(Self.rando_default)
+		(1 ... 9).forEach { _ in
+			result.append(UUID())
+		}
+		return result
+	}()
+	@State private var rando_spotlighted: UUID = Self.rando_default // As of iOS 18.3 developer beta 1, this breaks if we use type `UUID?`; `TabView` always selects the first tab.
+	private static let rando_default = UUID()
 }
 
 private struct RepNCAlbumTable: UIViewControllerRepresentable {
